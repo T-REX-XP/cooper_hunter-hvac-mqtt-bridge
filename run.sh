911b54c767ab69689611d091f2504ef02d5df993
T-REX-XP <4560084+T-REX-XP@users.noreply.github.com>
2020-09-11 18:35:16 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ MQTT_USERNAME=$(jq -r ".mqtt.username" $CONFIG_PATH)
 MQTT_PASSWORD=$(jq -r ".mqtt.password" $CONFIG_PATH)
 NAME=$(jq -r ".name" $CONFIG_PATH)
 
-npm install pm2@latest -g
+#npm install pm2@latest -g
 
 INSTANCES=$(jq '.devices | length' $CONFIG_PATH)
 
@@ -22,7 +22,7 @@ if [ "$INSTANCES" -gt 0 ]; then
 		if [[ $HVAC_HOST = null ]]; then echo "[ERROR] Missing hvac_host for device $i. Skipping." && continue; fi
 		if [[ $MQTT_TOPIC_PREFIX = null ]]; then echo "[ERROR] Missing mqtt_topic_prefix for device $i. Skipping." && continue; fi
 		echo "Running instance $i for $HVAC_HOST"
-		pm2 start index.js --silent -m --merge-logs --name="HVAC_${i}" -- \
+		node  index.js --silent -m --merge-logs --name="HVAC_${i}" -- \
 			--hvac-host="${HVAC_HOST}" \
 			--mqtt-broker-url="${MQTT_BROKER_URL}" \
 			--mqtt-topic-prefix="${MQTT_TOPIC_PREFIX}" \
